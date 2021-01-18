2a8bb149496b9612af28589bc223179ce775c45f
Robert Lindin <robert.lindin@gmail.com>
2021-01-18 01:37:48 +0100

    https support
    Replaced all http links with https.

--- javascripts/vendor/plugins/jquery.unparam.js
+++ javascripts/vendor/plugins/jquery.unparam.js
@@ -4,7 +4,7 @@
 
  * Copyright 2012 - https://github.com/yeikos/jquery.unparam
  * GNU General Public License
- * http://www.gnu.org/licenses/gpl-3.0.txt
+ * https://www.gnu.org/licenses/gpl-3.0.txt
  */
 
 
